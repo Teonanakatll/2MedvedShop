bc8dbe6702b978dd011c43e9dc3fc9b8757e0e42
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-05-12 21:57:15 +0300

    #1.2_______Модель, джанго-админка, форма

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
-def landing(request):
+from .forms import SubscriberForm
 
+
+def landing(request):
+    form = SubscriberForm(request.POST or None)
+
+    # cleaned_data можно получить только после проверки is_valid()
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        form.save()
     return render(request, 'landing/landing.html', locals())
