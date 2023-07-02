e0dd6c79c9f4fe93a5cae152b8b719c0183fe628
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-07-02 13:55:43 +0300

    #__________Добавляем настройки settings Debug, data.json и подсказки

--- medved2/settings.py
+++ medved2/settings.py
@@ -134,3 +134,9 @@ STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+if DEBUG == False:
+    try:
+        from .settings_prod import *
+    except:
+        pass
